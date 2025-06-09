56e9a894346eabee42e89776bcfdfe085da002d0
fratim <43808949+fratim@users.noreply.github.com>
2025-06-09 13:12:14 +0000

    Deploying to gh-pages from @ fratim/website_simple@cf72d346f5276ed2af1a5abc261c4a3ce00cbe83 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,41 +7,41 @@ ninja.data = [{
     title: "about",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/~frtim/";
+      window.location.href = "/https://username.github.io/";
     },
   },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/~frtim/blog/";
+            window.location.href = "/https://username.github.io/blog/";
           },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/~frtim/repositories/";
+            window.location.href = "/https://username.github.io/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/~frtim/cv/";
+            window.location.href = "/https://username.github.io/cv/";
           },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/~frtim/people/";
+            window.location.href = "/https://username.github.io/people/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/~frtim/books/the_godfather/";
+              window.location.href = "/https://username.github.io/books/the_godfather/";
             },},{id: "news-new-website-is-live",
           title: 'New website is live!',
           description: "",
@@ -73,47 +73,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/1_project/";
+              window.location.href = "/https://username.github.io/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/2_project/";
+              window.location.href = "/https://username.github.io/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/3_project/";
+              window.location.href = "/https://username.github.io/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/4_project/";
+              window.location.href = "/https://username.github.io/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/5_project/";
+              window.location.href = "/https://username.github.io/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/6_project/";
+              window.location.href = "/https://username.github.io/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/7_project/";
+              window.location.href = "/https://username.github.io/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/8_project/";
+              window.location.href = "/https://username.github.io/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/~frtim/projects/9_project/";
+              window.location.href = "/https://username.github.io/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
